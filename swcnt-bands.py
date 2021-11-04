ae384036aac4c8b835fa0024699703b95b9a3eea
tentacolo <tenobaldi@gmail.com>
2021-11-04 13:12:04 +0800

    update swcnt-bands

--- swcnt-bands.py
+++ swcnt-bands.py
@@ -1,3 +1,22 @@
+"""Pollo."""
 import numpy as np
 
-print("polli")
+
+def pollo(k, a1, a2):
+    """Pollo."""
+    band = np.sqrt(
+        3
+        + 2 * np.cos(np.dot(k, a1))
+        + 2 * np.cos(np.dot(k, a2))
+        + 2 * np.cos(np.dot(k, (a2 - a1)))
+    )
+    return band
+
+
+print("pol")
+
+k = 10
+a1 = 30
+a2 = 24
+
+print(pollo(k, a1, a2))
